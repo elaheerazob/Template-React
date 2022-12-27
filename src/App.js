24e772a56b8661ae4119db04e38e4c7e6b6d9d5d
elaheerazob <ridwanelahee282@gmail.com>
2022-12-27 22:23:35 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className='h-screen flex items-center justify-center bg-red-300'>
+      <h1 className='text-4xl from-gray-900 font-bold'>React Template</h1>
+    </div>
+  );
+}
+
+export default App;
